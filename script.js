eb7e55daa835b14933a643b73a5b0197cb11473e
Kriv Jhajhria <105179164+Glitchood@users.noreply.github.com>
2024-09-17 20:46:10 -0700

    ♻️ Refactor game logic for better structure

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ function tC(str) {
 }
 
 function getComputerChoice() {
-    
     let x = Math.random()
     x = Math.round(x * 10)
-    if (x <= 3) {
+    if (x <= 1/3) {
         return "rock";
-    } else if (x > 3 && x <= 6) {
+    } else if (x > 1/3 && x <= 2/3) {
         return "paper";
     } else {
         return "scissors";
@@ -19,12 +18,13 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let choice = prompt("What do you want to play? (Rock/Paper/Scissors)")
+    const viable_list = ['rock', 'paper', 'scissors']
     choice = choice.toLowerCase()
+    while (viable_list.indexOf(choice) < 0) {
+        choice = prompt(`${choice} isn't Rock, Paper, or Scissors- Try again`)
+    };
     return choice
 }
-
-let humanScore = 0
-let computerScore = 0
 
 function beats(humanChoice, computerChoice) {
     const beatList = [["Paper", "Rock"], ["Rock", "Scissors"], ["Scissors", "Paper"]];
@@ -37,21 +37,24 @@ function beats(humanChoice, computerChoice) {
     }
 
 function playRound(humanChoice, computerChoice) {
-    const viable_list = ['rock', 'paper', 'scissors']
-    if (viable_list.indexOf(humanChoice) < 0) {
-        console.log(`${humanChoice} isn't Rock, Paper, or Scissors- Try again`)
-        return
-    }
     console.log(`You played ${tC(humanChoice)}`)
     console.log(`Computer played ${tC(computerChoice)}`)
     if (humanChoice == computerChoice){
         console.log(`You tied! ${tC(humanChoice)} ties ${tC(computerChoice)}`)
+        return 0 // Tie case
     }
     else {
         const result = beats(humanChoice, computerChoice);
         console.log(`${result[0]} ${result[1]} beats ${result[2]}`)
+        if (humanChoice == result[1]){
+            return 1 // Human win case
+        }
+        else {
+            return 2 // Computer win case
+        }
     }
 }
+
 
 const computerChoice = getComputerChoice()
 const humanChoice = getHumanChoice()
